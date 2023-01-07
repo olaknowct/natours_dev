13bbf6bb0b6ac3da794185af0c4f877e665d2814
Christopher <olanochristopher@gmail.com>
2023-01-07 13:30:48 +0800

    Reading file from top level code. add get tours request

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
 const port = 3000;
 
+// Top level code = only execited pmce
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
 app.get('/', (req, res) => {
   // res.status(200).send('Hello from the server side');
   res
