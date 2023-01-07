579b796e6ec03fc2aa3560f10d9a28b2f0611aa0
Christopher <olanochristopher@gmail.com>
2023-01-07 21:40:27 +0800

    patch req added

--- app.js
+++ app.js
@@ -87,6 +87,16 @@ app.get('/', (req, res) => {
     .json({ message: 'Hello from the server side', app: 'Natours' });
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  if (id > tours.length)
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+
+  res
+    .status(200)
+    .json({ status: 'succes', data: { tour: '<Updated Tour Here...>' } });
+});
+
 app.listen(port, () => {
   console.log(`app running on port ${port}...`);
 });
