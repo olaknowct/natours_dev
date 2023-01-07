4d89815aa039341be9337182aa64e722302063ac
Christopher <olanochristopher@gmail.com>
2023-01-07 13:59:31 +0800

    Testing middleware and posts request

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const express = require('express');
 
 const app = express();
 
+// Middleware - a function that can modify the incoming request data
+// - it stands between request and response
+// - the data from the body is added to to request object
+// - midle ware express injected the data body from the req.body object
+app.use(express.json());
+
 const port = 3000;
 
-// Top level code = only execited pmce
+// Top level code = only executed once
+// what if the data gets updated does the server restarts?
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -19,6 +26,12 @@ app.get('/api/v1/tours', (req, res) => {
     },
   });
 });
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.end('done');
+});
+
 app.get('/', (req, res) => {
   // res.status(200).send('Hello from the server side');
   res
