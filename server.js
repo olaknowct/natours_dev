621c13eb868fa888fb3ef6060d9b0e2065caa2be
Christopher <olanochristopher@gmail.com>
2023-01-09 22:31:32 +0800

    use mongoose for database mongodb connection

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 // not related to express only the server
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+// this one execute first before require app since from the app we are defining the env
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
+// Replacing password
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+dbConnect();
+
+async function dbConnect() {
+  try {
+    const con = await mongoose.connect(DB);
+    console.log(con);
+    return con;
+  } catch (error) {
+    console.log(error);
+  }
+}
 // Express
 // console.log(app.get('env'));
 // console.log(process.env);
