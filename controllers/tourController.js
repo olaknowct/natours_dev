dc752ad3bf44dd4211b34f6830efc93898c087f7
Christopher <olanochristopher@gmail.com>
2023-01-11 14:58:36 +0800

    update api handler : delete tour api endpoint

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -143,6 +143,14 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({ status: 'succes', data: null });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({ status: 'succes', data: null });
+  } catch (error) {
+    res.status(400).json({
+      status: 'failed',
+      message: error.message,
+    });
+  }
 };
