192323081746d30f50dbee1f1aaac22eb0b5bed1
Christopher <olanochristopher@gmail.com>
2023-01-14 14:14:40 +0800

    Create Class for Global error utils

---
+++ utils/appError.js
@@ -0,0 +1,16 @@
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+
+    this.statusCode = statusCode;
+    // 400 - fail, 500 - error
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    // operational error - dev can predit, programming error - dev cant predit
+    this.isOperational = true;
+
+    // capturing where the error originated
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = AppError;
