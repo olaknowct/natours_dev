0a3e4bcbeb5816ac0e78b2ed05f526a754d15b88
Christopher <olanochristopher@gmail.com>
2023-01-16 14:22:24 +0800

    add default error status code

--- utils/appError.js
+++ utils/appError.js
@@ -2,7 +2,7 @@ class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
 
-    this.statusCode = statusCode;
+    this.statusCode = statusCode || 500;
     // 400 - fail, 500 - error
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     // operational error - dev can predit, programming error - dev cant predit
