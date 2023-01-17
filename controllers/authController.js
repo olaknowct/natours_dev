c83c68831791fb57e77796a88d8c7e32b79a9d52
Christopher <olanochristopher@gmail.com>
2023-01-17 20:20:01 +0800

    enhance sign up and make it more secured

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,15 @@ const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const { name, email, password, passwordConfirm } = req.body;
+  // input only the data required. this help the code more secured.
+  // ex. we are not allowin them to be an admin since its selected data only
+  const newUser = await User.create({
+    name,
+    email,
+    password,
+    passwordConfirm,
+  });
 
   res.status(201).json({
     status: 'success',
