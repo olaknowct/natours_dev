669c7f8a167623bcba096763040e39c63c78cbba
Christopher <olanochristopher@gmail.com>
2023-01-17 14:05:54 +0800

    add get users endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,17 @@
-exports.getAllUsers = (req, res) => {
-  res
-    .status(500)
-    .json({ status: 'error', message: 'This route is not yet defined!' });
-};
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res) => {
+  const users = await User.find();
+  0;
+  res.status(500).json({
+    status: 'error',
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+});
 
 exports.getUser = (req, res) => {
   res
