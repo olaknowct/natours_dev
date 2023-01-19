d85c785e6597ea0c111ac3b3f47f797ec22146b5
Christopher <olanochristopher@gmail.com>
2023-01-19 23:25:03 +0800

    Use mongo sanitize for nosql injection Use xss clean for html injection

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -34,6 +36,13 @@ app.use('/api', limiter);
 // - midle ware express injected the data body from the req.body object
 // Body Parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// after reading, then clean
+// data sanitization againts nosql query injection
+app.use(mongoSanitize());
+
+// data sanitization againts XSS, injection httml
+app.use(xss());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
