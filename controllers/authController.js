e56c808e82f77188e967af0892affebbaeed7a2e
Christopher <olanochristopher@gmail.com>
2023-01-19 22:15:37 +0800

    Send JWT via Cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,6 +15,20 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, res) => {
   // 1st paramter: payload - all object data you want to store
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 1000
+    ), //converrt to ms
+    // cookie cannot be accessed or modified in anyway by the browser in order to prevent cross site scripting attacks
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // Removed the password from response, it will not persist since we didnt not save after.
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
