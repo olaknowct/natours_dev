737b7883b8ced0003ac03fe68396f41445e00ae7
Christopher <olanochristopher@gmail.com>
2023-01-24 11:00:39 +0800

    create update one factory handler

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -19,3 +19,28 @@ exports.deleteOne = (Model) =>
 //   }
 //   res.status(204).json({ status: 'succes', data: null });
 // });
+
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({ status: 'succes', data: { doc } });
+  });
+
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+    res.status(201).json({
+      status: 'Success',
+      data: {
+        data: doc,
+      },
+    });
+  });
