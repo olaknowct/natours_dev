f3e28e3a8a581141412e309555a23aeff1828e50
Christopher <olanochristopher@gmail.com>
2023-02-01 17:14:19 +0800

    protecting get tour routes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,13 +4,14 @@ const {
   getTour,
   getLoginForm,
 } = require('../controllers/viewsController');
+const { protect } = require('../controllers/authController');
 
 const router = express.Router();
 
 router.get('/', getOverview);
 
 router.get('/tour', getTour);
-router.get('/tour/:slug', getTour);
+router.get('/tour/:slug', protect, getTour);
 router.get('/login', getLoginForm);
 
 module.exports = router;
