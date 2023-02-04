cdcbd02f792b5d3f0ad80a7ec67a0bdb13eb91bb
Christopher <olanochristopher@gmail.com>
2023-02-04 11:07:00 +0800

    setting its multer storage and filter saving image name to db

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,28 @@ const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
-const upload = multer({ dest: 'public/img/users' });
+// store it to file system
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/img/users');
+  },
+  filename: (req, file, cb) => {
+    // user-id-timestap.jpg
+    const ext = file.mimetype.split('/')[1];
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  },
+});
+
+// to test if the uploaded file is an image
+const multerFilter = (req, file, cb) => {
+  // mime type can detect what type of file, csv,
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please Upload only images.', 400), false);
+  }
+};
+const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -48,6 +69,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   }
   // filtered out unwanted fields names that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
   // Update user doc
   // we only need to update a certain field so dont punt req,body on 2nd argument: filter the body
   // .save iis not advisable to user since we dont like some other fields to be validated by our schema
