d183631fe04268523fa00bc3a12840da2574599d
Christopher <olanochristopher@gmail.com>
2023-02-04 20:31:24 +0800

    Building Email class handler

--- utils/email.js
+++ utils/email.js
@@ -1,33 +1,60 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async (options) => {
+// new Email(user, url).sendWelcome() // url can be a reset link
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Christopher Olano <${process.env.EMAIL_FROM}>`;
+  }
+
   // Create a transporter - a service that actually send the email
   // its not node js to send the email, its the service we define here
-  const transporter = nodemailer.createTransport({
-    // Gmail is not at all good idea for a production app but we can use
-    // gmail can only send 500 emails per day:jonas
-    // once exceeded it will be marked as a spam
-    // service: 'Gmail',
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-    // Activate in Gmail "less secure app"
-    // fakes to send emails to real addresses : mailtraps
-  });
-  // Define the email options
-  const mailOptions = {
-    from: ' Christopher Olano <test@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html
-  };
 
-  // Send the email
-  await transporter.sendMail(mailOptions);
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      return 1;
+    }
+
+    return nodemailer.createTransport({
+      // Gmail is not at all good idea for a production app but we can use
+      // gmail can only send 500 emails per day:jonas
+      // once exceeded it will be marked as a spam
+      // service: 'Gmail',
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+      // Activate in Gmail "less secure app"
+      // fakes to send emails to real addresses : mailtraps
+    });
+  }
+
+  async send(template, subject) {
+    // Render HTML based on the pug template
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      { firstName: this.firstName, url: this.url, subject }
+    );
+    // Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      text: htmlToText.fromString(html),
+      html,
+    };
+
+    // Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the natours family');
+  }
 };
-
-module.exports = sendEmail;
