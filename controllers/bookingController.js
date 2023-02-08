05abd7ec8400a71766cdb8a5edca3fd9325f058e
Christopher <olanochristopher@gmail.com>
2023-02-08 23:06:40 +0800

    fix event type typo

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -83,7 +83,7 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error: ${error}`);
   }
 
-  if (event.type === 'checkout.session.complete') {
+  if (event.type === 'checkout.session.completed') {
     createBookingCheckout(event.data.object);
   }
   res.status(200).json({ received: true });
