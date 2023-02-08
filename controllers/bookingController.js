13412688b8bd7b75ea09f4c065f13cd82331a51b
Christopher <olanochristopher@gmail.com>
2023-02-08 23:22:58 +0800

    update amount

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -54,7 +54,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 exports.createBookingCheckout = catchAsync(async (req, res, next) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 });
 
