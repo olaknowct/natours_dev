da0bd596cd42db488fce2d1dff0c1e0acd7c21fd
Christopher <olanochristopher@gmail.com>
2023-02-08 22:06:57 +0800

    add cors package and use it

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -34,6 +35,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Global Middlewares
+app.use(cors());
+
+app.options('*', cors());
+
 // Helmet - Security HTTP headers
 // Further HELMET configuration for Security Policy (CSP)
 // app.use(helmet());
